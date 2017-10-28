82aec50fa24d81b303df5a9ca8fc46e22055fdb4
smkang <ehfl1230@gmail.com>
2017-10-28 18:42:42 +0900

    expandableList까지

--- app/src/main/java/com/example/soomin/contestproject/Fragment3.java
+++ app/src/main/java/com/example/soomin/contestproject/Fragment3.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -21,53 +22,22 @@ import java.util.ArrayList;
  */
 
 public class Fragment3 extends Fragment{
-    ArrayList<RecordItemVO> datas;
-    ListView listView;
+    ArrayList<RecordItemVO> mGroupList;
+    ArrayList<ArrayList<RecordItemVO>> mChildList;
+    ArrayList<RecordItemVO> mChildListContent;
+    ExpandableListView listView;
     FloatingActionButton fab;
     RecordListAdapter adapter;
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        DBHelper helper = new DBHelper(getContext());
-        SQLiteDatabase db = helper.getWritableDatabase();
-        Cursor cursor = db.rawQuery("select * from medical_record order by date", null);
-
-        datas = new ArrayList<>();
-        while (cursor.moveToNext()) {
-            RecordItemVO vo = new RecordItemVO();
-            vo._id = cursor.getInt(0);
-            vo.title = cursor.getString(1);
-            vo.content = cursor.getString(2);
-            vo.name = cursor.getString(3);
-            vo.date = cursor.getString(4);
-            vo.dong_name = cursor.getString(5);
-            vo.dong_tel = cursor.getString(6);
-            vo.dong_address = cursor.getString(7);
-            vo.type = cursor.getString(8);
-            datas.add(vo);
-        }
-        adapter = new RecordListAdapter(this.getContext(), R.layout.record_list_item, datas);
-
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                String item_id = Integer.toString(datas.get(position)._id);
-                Intent intent = new Intent(Fragment3.super.getActivity(), RecordItemActivity.class);
-                intent.putExtra("item_id", item_id);
-                startActivity(intent);
-            }
-        });
-    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment3, container, false);
-        listView = (ListView) viewGroup.findViewById(R.id.record_list);
+        listView = (ExpandableListView) viewGroup.findViewById(R.id.record_list);
         fab = (FloatingActionButton) viewGroup.findViewById(R.id.add_btn);
+        mGroupList = new ArrayList<>();
+        mChildList = new ArrayList<ArrayList<RecordItemVO>>();
+        mChildListContent = new ArrayList<RecordItemVO>();
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -82,4 +52,41 @@ public class Fragment3 extends Fragment{
 
         return viewGroup;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        DBHelper helper = new DBHelper(getContext());
+        SQLiteDatabase db = helper.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select * from medical_record order by date", null);
+
+        while (cursor.moveToNext()) {
+
+            RecordItemVO vo = new RecordItemVO();
+            vo._id = cursor.getInt(0);
+            vo.title = cursor.getString(1);
+            vo.date = cursor.getString(4);
+            vo.dong_name = cursor.getString(5);
+            vo.type = cursor.getString(8);
+            mGroupList.add(vo);
+
+            vo.content = cursor.getString(2);
+            vo.name = cursor.getString(3);
+            vo.dong_tel = cursor.getString(6);
+            vo.dong_address = cursor.getString(7);
+            mChildListContent.add(vo);
+            mChildList.add(mChildListContent);
+        }
+        db.close();
+        adapter = new RecordListAdapter(this.getContext(), R.layout.record_list_item, R.layout.record_list_item, mGroupList, mChildList);
+        listView.setAdapter(adapter);
+        listView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                return false;
+            }
+
+        });
+    }
+
 }
--- app/src/main/java/com/example/soomin/contestproject/RecordListAdapter.java
+++ app/src/main/java/com/example/soomin/contestproject/RecordListAdapter.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseExpandableListAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.TimePicker;
@@ -20,28 +21,61 @@ import java.util.Calendar;
  * Created by SOOMIN on 2017-10-24.
  */
 
-public class RecordListAdapter extends ArrayAdapter<RecordItemVO> {
+public class RecordListAdapter extends BaseExpandableListAdapter {
     Context context;
-    ArrayList<RecordItemVO> datas;
+    ArrayList<RecordItemVO> groupList;
+    ArrayList<ArrayList<RecordItemVO>> childList;
     int resId;
+    int resId2;
     ImageView modifyBtnView = null;
 
 
-    public RecordListAdapter(Context context, int resId, ArrayList<RecordItemVO> datas) {
-        super(context, resId);
+    public RecordListAdapter(Context context, int resId, int resId2, ArrayList<RecordItemVO> groupList,  ArrayList<ArrayList<RecordItemVO>> childList) {
+        super();
         this.context = context;
-        this.datas = datas;
+        this.groupList = groupList;
+        this.childList = childList;
         this.resId = resId;
+        this.resId2 = resId2;
     }
 
     @Override
-    public int getCount() {
-        return datas.size();
+    public RecordItemVO getGroup(int groupPosition) {
+        return groupList.get(groupPosition);
+    }
+    @Override
+    public int getGroupCount() {
+        return groupList.size();
+    }
+    // 그룹 ID를 반환한다.
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
     }
 
+
+
+    @Override
+    public RecordItemVO getChild(int groupPosition, int childPosition) {
+        return childList.get(groupPosition).get(childPosition);
+    }
+
+    // 차일드뷰 사이즈를 반환한다.
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return childList.get(groupPosition).size();
+    }
+
+    // 차일드뷰 ID를 반환한다.
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
     @NonNull
     @Override
-    public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    public View getGroupView(int groupPosition, boolean isExpanded,
+                             View convertView, ViewGroup parent) {
+
         if (convertView == null) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflater.inflate(resId, null);
@@ -54,8 +88,8 @@ public class RecordListAdapter extends ArrayAdapter<RecordItemVO> {
         TextView dateView = wrapper.dateView;
         TextView titleView = wrapper.titleView;
         modifyBtnView = wrapper.modifyBtnView;
-        final RecordItemVO vo=datas.get(position);
-        titleView.setText(vo.title);
+        final RecordItemVO vo=groupList.get(groupPosition);
+       // titleView.setText(vo.title);
         dateView.setText(vo.date);
         SetOnClick(modifyBtnView, vo._id);
         return convertView;
@@ -70,4 +104,33 @@ public class RecordListAdapter extends ArrayAdapter<RecordItemVO> {
             }
         });
     }
+    @Override
+    public View getChildView(int groupPosition, int childPosition,
+                             boolean isLastChild, View convertView, ViewGroup parent) {
+
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(resId2, null);
+
+            RecordListWrapper wrapper = new RecordListWrapper(convertView);
+            convertView.setTag(wrapper);
+        }
+
+        RecordListWrapper wrapper = (RecordListWrapper) convertView.getTag();
+        TextView dateView = wrapper.dateView;
+        TextView titleView = wrapper.titleView;
+        modifyBtnView = wrapper.modifyBtnView;
+        final RecordItemVO vo=childList.get(groupPosition).get(childPosition);
+        titleView.setText(vo.title);
+        dateView.setText(vo.date);
+        return convertView;
+    }
+
+    @Override
+    public boolean hasStableIds() { return true; }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) { return true; }
+
+
 }
